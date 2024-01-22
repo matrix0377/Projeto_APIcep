869012a626e054a5b3369cc97a8c960459d0b962
Davi Augusto <davi.cianorte@gmail.com>
2024-01-22 19:52:57 -0300

    Inserindo projeto

---
+++ demo-rest-template/src/test/java/com/matrix0377/demoresttemplate/DemoRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix0377.demoresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
